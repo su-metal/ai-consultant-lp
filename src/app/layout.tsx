4209f887a0aa5dfc2b252a89cae78f1306976494
su-metal <117486217+su-metal@users.noreply.github.com>
2026-03-01 08:53:50 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,7 +62,7 @@ export const metadata: Metadata = {
     },
   },
   verification: {
-    google: 'verification=5XEVbrsXhLYYjSzJG2hLbhe_VLnqJsodV5JKtUrmqYI', // TODO: Google Search Console で取得したIDを設定
+    google: '5XEVbrsXhLYYjSzJG2hLbhe_VLnqJsodV5JKtUrmqYI', // Google Search Console で取得したIDのみを設定
   },
 
 
